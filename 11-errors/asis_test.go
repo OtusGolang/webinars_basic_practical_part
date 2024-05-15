ff3d13a965f23847cf587d3f552a1183f08bbdba
Alex Romanovskii <alex.romanovskii@resolver.com>
2024-05-15 15:18:25 -0400

    add code samples for "errors" lesson

---
+++ 11-errors/asis_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var ErrInvalidValue = errors.New("invalid value")
+
+func TestIs(t *testing.T) {
+	var err error
+	err = ErrInvalidValue
+	err = fmt.Errorf("wrapped: %w", err)
+
+	// ---
+
+	isWorks := errors.Is(err, ErrInvalidValue)
+	assert.True(t, isWorks)
+}
+
+type ErrorWithTimeout interface {
+	error
+	Timeout() bool
+}
+
+func TestIsAs_CustomType(t *testing.T) {
+	var err error
+	err = &os.PathError{Op: "open", Path: "file.txt", Err: errors.New("file not found")}
+	err = fmt.Errorf("wrapped: %w", err)
+
+	// ---
+
+	isWorks := errors.Is(err, &os.PathError{})
+	assert.False(t, isWorks)
+
+	var pathErr *os.PathError
+	asWorks := errors.As(err, &pathErr)
+	assert.True(t, asWorks)
+
+	var ifaceWorks ErrorWithTimeout
+	asWorksWithIface := errors.As(err, &ifaceWorks)
+	assert.True(t, asWorksWithIface)
+
+}
